b0e20e314b7a95ff07b38923fa9d30b10eab43f2
dpoqb <lzz@dpoqb.top>
2024-03-12 11:22:17 +0800

    添加了删除运行程序时的报错日志，添加了注释

--- Tiny C Compiler/tempUninstall.bat
+++ Tiny C Compiler/tempUninstall.bat
@@ -2,12 +2,15 @@
 title 卸载
 mode con cols=40 lines=10&color 3F
 cd /d %~dp0
-set /p uninstalldir=<"%temp%\TinyCCompiler\uninstalldir.log"
-del /f /s /q "%temp%\TinyCCompiler\uninstalldir.log" 1>NUL 2>NUL
+rem 从临时文件读取TCC安装路径
+set /p uninstalldir=<"%temp%\TinyCCompiler_uninstalldir.log"
+del /f /s /q "%temp%\TinyCCompiler_uninstalldir.log" 1>NUL 2>NUL
+rem 删除使用程序时产生的编译报错日志
+del /f /q "%temp%\TCC_Error.log" 1>NUL 2>NUL
 rd /s /q "%uninstalldir%" 1>NUL 2>NUL
 echo.
 echo 	卸载完成！
 echo.
 pause
-del /f %0
+del /f /q %0 1>NUL 2>NUL
 exit
